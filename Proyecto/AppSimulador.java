ef75c55274227b0d77d546c8ee9c465518f03b89
0achance0 <lnbatr5@gmail.com>
2024-11-11 08:25:59 -0700

    Update AppSimulador.java

--- Proyecto/AppSimulador.java
+++ Proyecto/AppSimulador.java
@@ -68,6 +68,37 @@ public class AppSimulador{
 			switch (opcionAlgoritmo) {
 				case 1:
 				RoundRobinApropiativo(procesos,quantum);
+				System.out.println("*");
+				System.out.println("Significado 'Estado actual': [(1) En ejecución] [(2) Listo] [(3) o (0) Bloqueado]"+"\n");
+				System.out.println("\t"+"\t"+"\t"+"Tabla de control de procesos (PCB)");
+				System.out.println("\t"+"Procesos en"+"\t"+"Tiempo restante  "+"\t"+"Estado actual"+"\t"+"Prioridad");
+				System.out.println("\t"+" ejecución "+"\t  "+"de ejecución");
+				for(int i=0; i<procesosEjecucion; i++){
+
+					if(procesos[i]!= null){
+						System.out.println("\t"+"     "+(i+1)+"\t"+"\t"+"\t"+procesos[i].getTiempoRestante()+"\t"+"\t"+"     "+procesos[i].getEstadoActual()+
+								"\t"+"\t"+"     "+procesos[i].getPrioridad());
+					}
+				}
+				for(int i=0; i<procesosEjecucion; i++){
+
+					if(procesos[i]!= null){
+
+						System.out.print("Entra Proceso ");
+						if(procesos[i].getEstadoActual()==3 || procesos[i].getEstadoActual()==0){
+							System.out.print((i+1)+", "+"no se ejecuta porque sigue bloqueado"+"\n");
+						}
+						else if(procesos[i].getEstadoActual()==1){
+							System.out.print((i+1)+", "+"se ejecuta"+"\n");
+						}
+						else if(procesos[i].getEstadoActual()==2){
+							System.out.print((i+1)+", "+"se ejecuta"+"\n");
+						}
+					}
+					else if(procesos[i]== null){
+						System.out.println("Entra Proceso "+(i+1)+", se ejecuta y termina");
+					}
+				}
 					break;
 
 				case 2:
@@ -84,6 +115,37 @@ public class AppSimulador{
 
 				case 5:
 				PlanificacionGarantizadaApropiativo();
+				System.out.println("*");
+				System.out.println("Significado 'Estado actual': [(1) En ejecución] [(2) Listo] [(3) o (0) Bloqueado]"+"\n");
+				System.out.println("\t"+"\t"+"\t"+"Tabla de control de procesos (PCB)");
+				System.out.println("\t"+"Procesos en"+"\t"+"Tiempo restante  "+"\t"+"Estado actual"+"\t"+"Prioridad");
+				System.out.println("\t"+" ejecución "+"\t  "+"de ejecución");
+				for(int i=0; i<procesosEjecucion; i++){
+
+					if(procesos[i]!= null){
+						System.out.println("\t"+"     "+(i+1)+"\t"+"\t"+"\t"+procesos[i].getTiempoRestante()+"\t"+"\t"+"     "+procesos[i].getEstadoActual()+
+								"\t"+"\t"+"     "+procesos[i].getPrioridad());
+					}
+				}
+				for(int i=0; i<procesosEjecucion; i++){
+
+					if(procesos[i]!= null){
+
+						System.out.print("Entra Proceso ");
+						if(procesos[i].getEstadoActual()==3 || procesos[i].getEstadoActual()==0){
+							System.out.print((i+1)+", "+"no se ejecuta porque sigue bloqueado"+"\n");
+						}
+						else if(procesos[i].getEstadoActual()==1){
+							System.out.print((i+1)+", "+"se ejecuta"+"\n");
+						}
+						else if(procesos[i].getEstadoActual()==2){
+							System.out.print((i+1)+", "+"se ejecuta"+"\n");
+						}
+					}
+					else if(procesos[i]== null){
+						System.out.println("Entra Proceso "+(i+1)+", se ejecuta y termina");
+					}
+				}
 					break;
 					
 				case 6:
@@ -103,6 +165,34 @@ public class AppSimulador{
 			switch (opcionAlgoritmo) {
 				case 1:
 				RoundRobinNoApropiativo(procesos,quantum);
+				System.out.println("*");
+				System.out.println(" Significado 'Estado actual': [(1) En ejecución] [(2) Listo] [(3) o (0)Bloqueado]"+"\n");
+				System.out.println("\t"+"\t"+"\t"+"Tabla de control de procesos (PCB)");
+				System.out.println("\t"+"Procesos en"+"\t"+"Tiempo restante  "+"\t"+"Estado actual"+"\t"+"Prioridad");
+				System.out.println("\t"+" ejecución "+"\t  "+"de ejecución");
+				for(int i=0; i<procesosEjecucion; i++){
+					if(procesos[i]!= null && procesos[i].getTiempoRestante() != 0){
+						System.out.println("\t"+"     "+(i+1)+"\t"+"\t"+"\t"+procesos[i].getTiempoRestante()+"\t"+"\t"+"     "+procesos[i].getEstadoActual()+
+								"\t"+"\t"+"     "+procesos[i].getPrioridad());
+					}
+				}
+				for(int i=0; i<procesosEjecucion; i++){
+					if(procesos[i]!= null){
+						System.out.print("Entra Proceso ");
+						if(procesos[i].getEstadoActual()==3 || procesos[i].getEstadoActual()==0){
+							System.out.print((i+1)+", "+"no se ejecuta porque sigue bloqueado"+"\n");
+						}
+						else if(procesos[i].getEstadoActual()==1){
+							System.out.print((i+1)+", "+"se ejecuta"+"\n");
+						}
+						else if(procesos[i].getEstadoActual()==2){
+							System.out.print((i+1)+", "+"se ejecuta"+"\n");
+						}
+					}
+					else if(procesos[i]== null){
+						System.out.println("Entra Proceso "+(i+1)+", se ejecuta y termina");
+					}
+				}
 					break;
 
 				case 2:
@@ -119,6 +209,36 @@ public class AppSimulador{
 
 				case 5:
 				PlanificacionGarantizadaNoApropiativo();
+				System.out.println("*");
+				System.out.println("Significado 'Estado actual': [(1) En ejecución] [(2) Listo] [(3) o (0) Bloqueado]"+"\n");
+				System.out.println("\t"+"\t"+"\t"+"Tabla de control de procesos (PCB)");
+				System.out.println("\t"+"Procesos en"+"\t"+"Tiempo restante  "+"\t"+"Estado actual"+"\t"+"Prioridad");
+				System.out.println("\t"+" ejecución "+"\t  "+"de ejecución");
+				for(int i=0; i<procesosEjecucion; i++){
+					if(procesos[i]!= null){
+						System.out.println("\t"+"     "+(i+1)+"\t"+"\t"+"\t"+procesos[i].getTiempoRestante()+"\t"+"\t"+"     "+procesos[i].getEstadoActual()+
+								"\t"+"\t"+"     "+procesos[i].getPrioridad());
+					}
+				}
+				for(int i=0; i<procesosEjecucion; i++){
+					if(procesos[i]!= null){
+						System.out.print("Entra Proceso ");
+						if(procesos[i].getEstadoActual()==3 || procesos[i].getEstadoActual()==0){
+							System.out.print((i+1)+", "+"no se ejecuta porque sigue bloqueado"+"\n");
+						}
+						else if(procesos[i].getEstadoActual()==1){
+							System.out.print((i+1)+", "+"se ejecuta"+"\n");
+						}
+						else if(procesos[i].getEstadoActual()==2){
+							System.out.print((i+1)+", "+"se ejecuta"+"\n");
+						}
+					}
+					else if(procesos[i]== null){
+
+						System.out.println("Entra Proceso "+(i+1)+", se ejecuta y termina");
+
+					}
+				}
 					break;
 					
 				case 6:
@@ -137,17 +257,193 @@ public class AppSimulador{
 
 	}
 	
+public static void RoundRobinApropiativo (Proceso [] procesos, int tiempoMonitoreoCPU, int quantum){
+//		[(1) En ejecución] [(2) Listo] [(3) Bloqueado]
+		int copiaTimeMonit=tiempoMonitoreoCPU;
+			
+			for(int i=0; i<procesos.length; i++){
 
-	public static void RoundRobinApropiativo (Proceso [] procesos, int quantum){
-	
-		
+				if(procesos[i]!= null && copiaTimeMonit >= 1){
+
+					if(procesos[i].getEstadoActual()==0 || procesos[i].getEstadoActual()==3){
+
+						procesos[i].setEstadoActual(((int)(Math.random()*2)));
+
+						if(procesos[i].getEstadoActual()==1){
+							procesos[i].setEstadoActual(2);
+						}
+					}
+					if(procesos[i].getEstadoActual()==1){
+
+						if(quantum>procesos[i].getTiempoRestante()){
+							if(copiaTimeMonit<procesos[i].getTiempoRestante()){
+								quantum=copiaTimeMonit;
+							}
+							quantum=procesos[i].getTiempoRestante();
+						}
+						procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-quantum);
+						if(procesos[i].getTiempoRestante()==0){
+							procesos[i]=null;
+							continue;
+						}
+					}
+					else if(procesos[i].getEstadoActual()==2){
+						procesos[i].setEstadoActual(1);
+						if(quantum>procesos[i].getTiempoRestante()){
+							if(copiaTimeMonit<procesos[i].getTiempoRestante()){
+								quantum=copiaTimeMonit;
+							}
+							quantum=procesos[i].getTiempoRestante();
+						}
+						procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-quantum);
+						if(procesos[i].getTiempoRestante()==0){
+							procesos[i]=null;
+							continue;
+						}
+					}
+				}
+				else{
+
+					break;
+				}
+			}
+			copiaTimeMonit-=quantum;
 	}
 	
-	public static void RoundRobinNoApropiativo (Proceso [] procesos, int quantum){
+	public static void RoundRobinNoApropiativo (Proceso [] procesos, int tiempoMonitoreoCPU, int quantum){
+//		[(1) En ejecución] [(2) Listo] [(3) Bloqueado]
+		int tiempoSeguido=0, copiaTimeMonit=tiempoMonitoreoCPU;
 		
-		
+		for(int i=0; i<procesos.length; i++){
+
+			if(procesos[i]!= null && copiaTimeMonit >= 1){
+
+				if(procesos[i].getEstadoActual()==0 || procesos[i].getEstadoActual()==3){
+
+					procesos[i].setEstadoActual(((int)(Math.random()*2)));
+
+					if(procesos[i].getEstadoActual()==1){
+						procesos[i].setEstadoActual(2);
+					}
+				}
+				if(procesos[i].getEstadoActual()==1){
+
+					tiempoSeguido=((int)(Math.random()*3)+2)*quantum;
+					System.out.println("+"+tiempoSeguido);
+
+					if(tiempoSeguido>procesos[i].getTiempoRestante() ){
+						if(copiaTimeMonit<procesos[i].getTiempoRestante()){
+							tiempoSeguido=copiaTimeMonit;
+						}
+						else{
+							tiempoSeguido=procesos[i].getTiempoRestante();
+						}
+						procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-tiempoSeguido);
+						copiaTimeMonit-=tiempoSeguido;
+						System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+						System.out.println("*"+copiaTimeMonit);
+						if(procesos[i].getTiempoRestante()==0){
+							procesos[i]=null;
+						}
+						if(copiaTimeMonit==0){
+							break;
+						}
+					}
+					else if(procesos[i].getTiempoRestante()>copiaTimeMonit){
+						
+						tiempoSeguido=copiaTimeMonit;
+						procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-tiempoSeguido);
+						copiaTimeMonit-=tiempoSeguido;
+						System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+						System.out.println("*"+copiaTimeMonit);
+						if(procesos[i].getTiempoRestante()==0){
+							procesos[i]=null;
+						}
+						if(copiaTimeMonit==0){
+							break;
+						}
+					}
+					else{
+						
+						procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-tiempoSeguido);
+						copiaTimeMonit-=tiempoSeguido;
+						System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+						System.out.println("*"+copiaTimeMonit);
+						if(procesos[i].getTiempoRestante()==0){
+							procesos[i]=null;
+						}
+						if(copiaTimeMonit==0){
+							break;
+						}
+					}
+					continue;
+				}
+				else if(procesos[i].getEstadoActual()==2){
+
+					tiempoSeguido=((int)(Math.random()*3)+2)*quantum;
+					System.out.println("++"+tiempoSeguido);
+					
+					if(tiempoSeguido>procesos[i].getTiempoRestante()){
+						if(copiaTimeMonit<procesos[i].getTiempoRestante()){
+							tiempoSeguido=copiaTimeMonit;
+						}
+						else{
+							tiempoSeguido=procesos[i].getTiempoRestante();
+						}
+						procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-tiempoSeguido);
+						copiaTimeMonit-=tiempoSeguido;
+						procesos[i].setEstadoActual(1);
+						System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+						if(procesos[i].getTiempoRestante()==0){
+							procesos[i]=null;
+						}
+						if(copiaTimeMonit==0){
+							break;
+						}
+					}
+					else if(procesos[i].getTiempoRestante()>copiaTimeMonit){
+						
+						tiempoSeguido=copiaTimeMonit;
+						procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-tiempoSeguido);
+						copiaTimeMonit-=tiempoSeguido;
+						procesos[i].setEstadoActual(1);
+						System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+						if(procesos[i].getTiempoRestante()==0){
+							procesos[i]=null;
+						}
+						if(copiaTimeMonit==0){
+							break;
+						}
+					}
+					else {
+
+						procesos[i].setEstadoActual(1);
+						procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-tiempoSeguido);
+						copiaTimeMonit-=tiempoSeguido;
+						System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+						if(procesos[i].getTiempoRestante()==0){
+							procesos[i]=null;
+						}
+						System.out.println("**"+copiaTimeMonit);
+						if(copiaTimeMonit==0){
+							break;
+						}
+						
+					}	
+					continue;
+				}
+			}
+			else{
+				
+				continue;
+			}
+			
+			if(procesos[i].getTiempoRestante()!=0){
+				
+				System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+			}
+		}
 	}
-	
 	public static void PrioridadesApropiativo (Proceso [] procesos, int quantum, int tiempoMonitoreoCPU){
 
 		int n = procesos.length;
@@ -248,14 +544,191 @@ public class AppSimulador{
 
 	}
 
-	public static void PlanificacionGarantizadaApropiativo (){
+	public static int PlanificacionGarantizadaApropiativo (Proceso [] procesos, int tiempoMonitoreoCPU, int quantum){
+//		[(1) En ejecución] [(2) Listo] [(3) Bloqueado]
+		int copiaTimeMonit=tiempoMonitoreoCPU, promesa=tiempoMonitoreoCPU/procesos.length;
+			
+			for(int i=0; i<procesos.length; i++){
 
+				if(procesos[i]!= null && copiaTimeMonit >= 1){
 
+					if(procesos[i].getEstadoActual()==0 || procesos[i].getEstadoActual()==3){
+
+						procesos[i].setEstadoActual(((int)(Math.random()*2)));
+
+						if(procesos[i].getEstadoActual()==1){
+							procesos[i].setEstadoActual(2);
+						}
+					}
+					if(procesos[i].getEstadoActual()==1){
+
+						if(procesos[i].getTiempoRestante()<promesa){
+							if(copiaTimeMonit<procesos[i].getTiempoRestante()){
+								promesa=copiaTimeMonit;
+							}
+							promesa=procesos[i].getTiempoRestante();
+						}
+						procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-promesa);	
+						if(procesos[i].getTiempoRestante()==0){
+							procesos[i]=null;
+							continue;
+						}
+					}
+					else if(procesos[i].getEstadoActual()==2){
+
+						procesos[i].setEstadoActual(1);
+						if(procesos[i].getTiempoRestante()<promesa){
+							if(copiaTimeMonit<procesos[i].getTiempoRestante()){
+								promesa=copiaTimeMonit;
+							}
+							promesa=procesos[i].getTiempoRestante();
+						}
+						procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-promesa);	
+						if(procesos[i].getTiempoRestante()==0){
+
+							procesos[i]=null;
+							continue;
+						}
+					}
+				}
+				else{
+
+					break;
+				}
+			}
+			copiaTimeMonit-=quantum;
+			return promesa;
 	}
+	public static void PlanificacionGarantizadaNoApropiativo (Proceso [] procesos, int tiempoMonitoreoCPU, int quantum){
+//		[(1) En ejecución] [(2) Listo] [(3) Bloqueado]
+		int tiempoSeguido=0, copiaTimeMonit=tiempoMonitoreoCPU, promesa=tiempoMonitoreoCPU/procesos.length;
+			
+			for(int i=0; i<procesos.length; i++){
 
-	public static void PlanificacionGarantizadaNoApropiativo (){
+				if(procesos[i]!= null && copiaTimeMonit >= 1){
 
+					if(procesos[i].getEstadoActual()==0 || procesos[i].getEstadoActual()==3){
 
+						procesos[i].setEstadoActual(((int)(Math.random()*2)));
+
+						if(procesos[i].getEstadoActual()==1){
+							procesos[i].setEstadoActual(2);
+						}
+					}
+					if(procesos[i].getEstadoActual()==1){
+
+						tiempoSeguido=((int)(Math.random()*3)+2)*promesa;
+						System.out.println("+"+tiempoSeguido);
+				
+						if(tiempoSeguido>procesos[i].getTiempoRestante() ){
+							if(copiaTimeMonit<procesos[i].getTiempoRestante()){
+								tiempoSeguido=copiaTimeMonit;
+							}
+							else{
+								tiempoSeguido=procesos[i].getTiempoRestante();
+							}
+							procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-tiempoSeguido);
+							copiaTimeMonit-=tiempoSeguido;
+							System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+							System.out.println("*"+copiaTimeMonit);
+							if(procesos[i].getTiempoRestante()==0){
+								procesos[i]=null;
+							}
+							if(copiaTimeMonit==0){
+								break;
+							}
+						}
+						else if(procesos[i].getTiempoRestante()>copiaTimeMonit){
+							
+							tiempoSeguido=copiaTimeMonit;
+							procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-tiempoSeguido);
+							copiaTimeMonit-=tiempoSeguido;
+							System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+							System.out.println("*"+copiaTimeMonit);
+							if(procesos[i].getTiempoRestante()==0){
+								procesos[i]=null;
+							}
+							if(copiaTimeMonit==0){
+								break;
+							}
+						}
+						else{
+							
+							procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-tiempoSeguido);
+							copiaTimeMonit-=tiempoSeguido;
+							System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+							System.out.println("*"+copiaTimeMonit);
+							if(procesos[i].getTiempoRestante()==0){
+								procesos[i]=null;
+							}
+							if(copiaTimeMonit==0){
+								break;
+							}
+						}
+						continue;
+					}
+					else if(procesos[i].getEstadoActual()==2){
+
+						tiempoSeguido=((int)(Math.random()*3)+2)*promesa;
+						System.out.println("++"+tiempoSeguido);
+						
+						if(tiempoSeguido>procesos[i].getTiempoRestante()){
+							if(copiaTimeMonit<procesos[i].getTiempoRestante()){
+								tiempoSeguido=copiaTimeMonit;
+							}
+							else{
+								tiempoSeguido=procesos[i].getTiempoRestante();
+							}
+							procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-tiempoSeguido);
+							copiaTimeMonit-=tiempoSeguido;
+							procesos[i].setEstadoActual(1);
+							System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+							if(procesos[i].getTiempoRestante()==0){
+								procesos[i]=null;
+							}
+							if(copiaTimeMonit==0){
+								break;
+							}
+						}
+						else if(procesos[i].getTiempoRestante()>copiaTimeMonit){
+							
+							tiempoSeguido=copiaTimeMonit;
+							procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-tiempoSeguido);
+							copiaTimeMonit-=tiempoSeguido;
+							procesos[i].setEstadoActual(1);
+							System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+							if(procesos[i].getTiempoRestante()==0){
+								procesos[i]=null;
+							}
+							if(copiaTimeMonit==0){
+								break;
+							}
+						}
+						else {
+
+							procesos[i].setEstadoActual(1);
+							procesos[i].setTiempoRestante(procesos[i].getTiempoRestante()-tiempoSeguido);
+							copiaTimeMonit-=tiempoSeguido;
+							System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+							if(procesos[i].getTiempoRestante()==0){
+								procesos[i]=null;
+							}
+							System.out.println("**"+copiaTimeMonit);
+							if(copiaTimeMonit==0){
+								break;
+							}
+						}	
+						continue;
+					}
+				}
+				else{
+					
+					continue;
+				}
+				if(procesos[i].getTiempoRestante()!=0){
+					System.out.println("\n"+"Proceso "+(i+1)+" quedará con tiempo restante de ejecución de: "+procesos[i].getTiempoRestante());
+				}
+			}
 	}
 
 	public static void BoletosLoteriaApropiativo (){
