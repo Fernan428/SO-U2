997185991540e318722553cd6b05652035fe3cbe
0achance0 <lnbatr5@gmail.com>
2024-11-11 08:40:59 -0700

    Update AppSimulador.java

--- Proyecto/AppSimulador.java
+++ Proyecto/AppSimulador.java
@@ -242,11 +242,11 @@ public class AppSimulador{
 					break;
 					
 				case 6:
-				BoletosLoteriaNoApropiativo();
+				BoletosLoteriaNoApropiativo(procesos, quantum, tiempoMonitoreoCPU);
 					break;
 
 				case 7:
-				ParticipacionEquitativaNoApropiativo();
+				ParticipacionEquitativaNoApropiativo(procesos, quantum, tiempoMonitoreoCPU);
 					break;
 			
 				default:
@@ -731,16 +731,277 @@ public static void RoundRobinApropiativo (Proceso [] procesos, int tiempoMonitor
 			}
 	}
 
-	public static void BoletosLoteriaApropiativo (){
-
-
+	public static void BoletosLoteriaApropiativo (Proceso [] procesos, int quantum, int tiempoMonitoreoCPU){
+		int boletos [] = new int [procesos.length];
+		int contUnidades [] = new int [procesos.length];
+		int tiempoRest = tiempoMonitoreoCPU, cantCambios = 0;
+		for (int i = 0; i < procesos.length; i++) {
+			switch(procesos[i].getPrioridad()) {
+			case 1:
+				boletos[i]=6;
+				break;
+			case 2: 
+				boletos[i]=8;
+				break;
+			case 3: 
+				boletos[i]=10;
+				break;
+			case 4: 
+				boletos[i]=12;
+				break;
+			default:
+				boletos[i]=4;
+			}
+		}
+		int totalBoletos = 0;
+		for (int i = 0; i < procesos.length; i++) {
+			if (procesos[i].getTiempoRestante() > 0) {
+				totalBoletos += boletos[i];
+			}
+		}
+		int loteria [] = new int[totalBoletos];
+		for (int i = 0; i < boletos.length; i++) {
+			int cant = boletos[i];
+			for (int j = 0; j < cant; j++) {
+				int numBol;
+				do {
+					numBol = (int) (Math.random() * loteria.length);
+				} while (loteria[numBol] != 0); 
+				loteria[numBol] = i; 
+			}
+		}
+		while (tiempoRest > 0) {
+	        int bolGan = (int) (Math.random() * loteria.length);
+	        int pos = loteria[bolGan];
+	        if (procesos[pos] == null || procesos[pos].getTiempoRestante() == 0 || loteria[bolGan] == 0) {
+	            continue;
+	        }
+	        if (procesos[pos].getEstadoActual() == 3) {
+	            int ejecuta = (int) (Math.random() * 2);
+	            if (ejecuta == 1) {
+	                procesos[pos].setEstadoActual(2);
+	            } else continue;
+	        }
+	        if (tiempoRest < quantum) {
+	            procesos[pos].setTiempoRestante(procesos[pos].getTiempoRestante() - tiempoRest);
+	            contUnidades[pos] += tiempoRest;
+	            tiempoRest = 0;
+	            cantCambios++;
+	        } else {
+	            tiempoRest -= quantum;
+	            if (procesos[pos].getTiempoRestante() - quantum < 0) {
+	                int cant = procesos[pos].getTiempoRestante();
+	                procesos[pos].setTiempoRestante(0);
+	                procesos[pos] = null;
+	                contUnidades[pos] += cant;
+	                cantCambios++;
+	            } else {
+	                procesos[pos].setTiempoRestante(procesos[pos].getTiempoRestante() - quantum);
+	                contUnidades[pos] += quantum;
+	                cantCambios++;
+	            }
+	        }
+			int procTerm = 0, bolUsados = 0;
+			for (int i = 0; i < procesos.length; i++) {
+				if (procesos[i] == null) procTerm++;
+			}
+			for (int i = 0; i < loteria.length; i++) {
+				if (loteria[i] == 0) bolUsados++;
+			}
+			if (procTerm == procesos.length || bolUsados == loteria.length) tiempoRest = 0;
+			loteria[bolGan] = 0;
+		}
+		System.out.println("Informe de uso de procesador con: Boletos de Lotería");
+		for (int i = 0; i < procesos.length; i++) {
+			if (procesos[i] == null) {
+				System.out.println("Entra Proceso " + (i + 1) + ", se ejecuta " + (contUnidades[i])+ " unidades y termina");
+			} else {
+				System.out.print("Entra Proceso ");
+				if (procesos[i].getEstadoActual() == 3 || procesos[i].getEstadoActual() == 0) {
+					System.out.print((i + 1) + ", no se ejecuto porque sigue bloqueado\n");
+				} else if (contUnidades[i] == 0){
+					System.out.print((i + 1) + ", no se ejecuto \n");
+				} else System.out.print((i + 1) + ", se ejecuta " + (contUnidades[i]) + " unidades \n");				
+			}
+		}
+		System.out.println("Informe final de ejecucion:");
+		System.out.print("Que procesos terminaron:");
+		int cont = 0;
+		for (int i = 0; i < procesos.length; i++) {
+			if (procesos[i] == null) {
+				System.out.print(" " + (i+1));
+				cont++;
+			}
+		}
+		if (cont == 0) {
+			System.out.print(" ninguno.");
+		}
+		System.out.println();
+		System.out.print("Cuales no entraron nunca en ejecucion:");
+		int aux = 0;
+		for (int i = 0; i < contUnidades.length; i++) {
+			if (contUnidades[i] == 0) {
+				System.out.print(" " + (i+1));
+				aux++;
+			}
+		}
+		if (aux == 0) {
+			System.out.print(" ninguno.");
+		}
+		System.out.println();
+		System.out.print("Cuantos siguen en proceso:");
+		int j = 0;
+		for (int i = 0; i < procesos.length; i++) {
+			if (procesos[i] == null ) continue;
+			if (procesos[i].getEstadoActual() == 2 || procesos[i].getEstadoActual() == 1) {
+				System.out.print(" " + (i+1));
+				j++;
+			}
+		}
+		if (j == 0) {
+			System.out.print(" ninguno.");
+		}
+		System.out.println();
+		System.out.println("Cantidad de cambios de procesos: " + cantCambios);
+		int totalEjecutado = 0;
+		for (int unidad : contUnidades) {
+			totalEjecutado += unidad;
+		}
+		System.out.println("Total de unidades ejecutadas: " + totalEjecutado);
+		System.out.println("Tiempo monitoreado del CPU: " + tiempoMonitoreoCPU);
 	}
 
-	public static void BoletosLoteriaNoApropiativo (){
+	public static void BoletosLoteriaNoApropiativo(Proceso[] procesos, int quantum, int tiempoMonitoreoCPU) {
+		int boletos[] = new int[procesos.length];
+		int contUnidades[] = new int[procesos.length];
+		int tiempoRest = tiempoMonitoreoCPU;
+		int cantCambios = 0;
+		for (int i = 0; i < procesos.length; i++) {
+			switch (procesos[i].getPrioridad()) {
+			case 1:
+				boletos[i] = 6;
+				break;
+			case 2:
+				boletos[i] = 8;
+				break;
+			case 3:
+				boletos[i] = 10;
+				break;
+			case 4:
+				boletos[i] = 12;
+				break;
+			default:
+				boletos[i] = 4;
+			}
+		}
+		int totalBoletos = 0;
+		for (int i = 0; i < procesos.length; i++) {
+			if (procesos[i].getTiempoRestante() > 0) {
+				totalBoletos += boletos[i];
+			}
+		}
+		int loteria[] = new int[totalBoletos];
+		for (int i = 0; i < boletos.length; i++) {
+			int cant = boletos[i];
+			for (int j = 0; j < cant; j++) {
+				int numBol;
+				do {
+					numBol = (int) (Math.random() * loteria.length);
+				} while (loteria[numBol] != 0);
+				loteria[numBol] = i;
+			}
+		}
+		while (tiempoRest > 0) {
+			int bolGan = (int) (Math.random() * loteria.length);
+			int pos = loteria[bolGan];
+			if (procesos[pos] == null || procesos[pos].getTiempoRestante() == 0 || loteria[bolGan] == 0) {
+				continue;
+			}
+			int tiempoUso = (int) ((Math.random() * 2) + 2);
+			if (tiempoUso > procesos[pos].getTiempoRestante()) {
+				tiempoUso = procesos[pos].getTiempoRestante();
+			}
+			if (tiempoUso > tiempoRest) {
+				tiempoUso = tiempoRest;
+			}
+			procesos[pos].setTiempoRestante(procesos[pos].getTiempoRestante() - tiempoUso);
+			contUnidades[pos] += tiempoUso;
+			tiempoRest -= tiempoUso;
+			cantCambios++;
+			int nuevoEstado = (int) (Math.random() * 3) + 1;
+			procesos[pos].setEstadoActual(nuevoEstado);
 
+			if (procesos[pos].getTiempoRestante() == 0) {
+				procesos[pos] = null;
+			}
+			int procTerm = 0;
+			for (int i = 0; i < procesos.length; i++) {
+				if (procesos[i] == null) procTerm++;
+			}
+			if (procTerm == procesos.length) tiempoRest = 0;
+			loteria[bolGan] = 0;
+		}
+		System.out.println("Informe de uso de procesador con: Boletos de Lotería (No Apropiativo)");
+		for (int i = 0; i < procesos.length; i++) {
+			if (procesos[i] == null) {
+				System.out.println("Entra Proceso " + (i + 1) + ", se ejecuta " + (contUnidades[i])+ " unidades y termina");
+			} else {
+				System.out.print("Entra Proceso ");
+				if (procesos[i].getEstadoActual() == 3 || procesos[i].getEstadoActual() == 0) {
+					System.out.print((i + 1) + ", no se ejecuto porque sigue bloqueado\n");
+				} else if (contUnidades[i] == 0){
+					System.out.print((i + 1) + ", no se ejecuto \n");
+				} else System.out.print((i + 1) + ", se ejecuta " + (contUnidades[i]) + " unidades \n");				
+			}
+		}
+		System.out.println("Informe final de ejecucion:");
+		System.out.print("Que procesos terminaron:");
+		int cont = 0;
+		for (int i = 0; i < procesos.length; i++) {
+			if (procesos[i] == null) {
+				System.out.print(" " + (i+1));
+				cont++;
+			}
+		}
+		if (cont == 0) {
+			System.out.print(" ninguno.");
+		}
+		System.out.println();
+		System.out.print("Cuales no entraron nunca en ejecucion:");
+		int aux = 0;
+		for (int i = 0; i < contUnidades.length; i++) {
+			if (contUnidades[i] == 0) {
+				System.out.print(" " + (i+1));
+				aux++;
+			}
+		}
+
+		if (aux == 0) {
+			System.out.println(" ninguno.");
+		}
+		System.out.println();
+		System.out.print("Cuantos siguen en proceso:");
+		int j = 0;
+		for (int i = 0; i < procesos.length; i++) {
+			if (procesos[i] == null ) continue;
+			if (procesos[i].getEstadoActual() == 2 || procesos[i].getEstadoActual() == 1) {
+				System.out.print(" " + (i+1));
+				j++;
+			}
+		}
+		if (j == 0) {
+			System.out.println(" ninguno.");
+		}
+		System.out.println();
+		System.out.println("Cantidad de cambios de procesos: " + cantCambios);
+		int totalEjecutado = 0;
+		for (int unidad : contUnidades) {
+			totalEjecutado += unidad;
+		}
+		System.out.println("Total de unidades ejecutadas: " + totalEjecutado);
+		System.out.println("Tiempo monitoreado del CPU: " + tiempoMonitoreoCPU);
 
 	}
-
 	public static void ParticipacionEquitativaApropiativo (){
 
 
